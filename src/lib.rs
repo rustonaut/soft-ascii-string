84052dd03fce97991e4e56ccd225f5a1d4f87f78
Philipp Korber <philippkorber@gmail.com>
2017-11-07 16:59:08 +0100

    doc: added crate level documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,23 @@
+//! This crate provides char, str and string wrappers which
+//! have an "is-ascii" soft constraint.
+//!
+//! As it is a soft constraint it can be violated, while a violation
+//! is (normally) a bug it _does not_ introduce any safety issues.
+//! In this soft-ascii-string differs to e.g. [ascii](https://crates.io/crates/ascii)
+//! which uses a hard constraint and where a violation does brake
+//! rust safety and potentially introduces undefined behaviour.
+//!
+//! Soft-ascii-string is suited for situations where many places
+//! (e.g. external libraries) output strings which should be
+//! ascii and which you do not want to iterate over to assure
+//! they are ascii but where you neither want to use a unsafe
+//! conversions as it would be required by the ascii crate.
+//!
+//! This crate is not necessarily suited if you want to rally on the string
+//! being ascii on a safety level, you might want to consider using
+//! [ascii](https://crates.io/crates/ascii) in that case.
+
+
 pub use self::soft_char::*;
 pub use self::soft_str::*;
 pub use self::soft_string::*;
